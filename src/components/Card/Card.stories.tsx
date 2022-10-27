4b0eeda971fb35758bb87c6a58f96fa05c73021c
Christopher <Chris1Bobsy@Gmail.com>
2022-10-27 23:52:19 +0100

    add to card story

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Card from "./Card";
+import Button from "../Button";
 
 export default {
   title: "ReactComponentLibrary/Card",
@@ -8,8 +9,42 @@ export default {
 } as ComponentMeta<typeof Card>;
 
 const Template: ComponentStory<typeof Card> = (args) => (
-  <Card {...args}>My name is kira yoshikage.</Card>
+  <Card {...args}>
+    <span style={{ marginBottom: "15px" }}>
+      My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast
+      section of Morioh, where all the villas are, and I am not married. I work
+      as an employee for the Kame Yu department stores, and I get home every day
+      by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed
+      by 11 PM, and make sure I get eight hours of sleep, no matter what. After
+      having a glass of warm milk and doing about twenty minutes of stretches
+      before going to bed, I usually have no problems sleeping until morning.
+      Just like a baby, I wake up without any fatigue or stress in the morning.
+      I was told there were no issues at my last check-up. I'm trying to explain
+      that I'm a person who wishes to live a very quiet life. I take care not to
+      trouble myself with any enemies, like winning and losing, that would cause
+      me to lose sleep at night. That is how I deal with society, and I know
+      that is what brings me happiness. Although, if I were to fight I wouldn't
+      lose to anyone.
+    </span>
+    <div style={{ display: "flex", width: "100%", justifyContent: "flex-end" }}>
+      <Button outlined size="small">
+        Click Me
+      </Button>
+    </div>
+  </Card>
 );
 
-export const Example = Template.bind({});
-Example.args = {};
+export const CardWithImage = Template.bind({});
+CardWithImage.args = {
+  imageSrc:
+    "https://images.unsplash.com/photo-1666884471882-cf4d4ca0fc2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  title: "Kira Yoshikage",
+};
+
+export const CardWithTitle = Template.bind({});
+CardWithTitle.args = {
+  title: "Kira Yoshikage",
+};
+
+export const SimpleCard = Template.bind({});
+SimpleCard.args = {};
